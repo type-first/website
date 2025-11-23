dad4fd43a69f610fbd1509a5e5c428ea293ea969
santiago-elustondo <elus2012@gmail.com>
2025-11-23 01:41:36 -0500

    typescapes

--- content/typescape/basic-types-and-functions/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Export chunks in a format that can be imported by registries
- */
-
-export { chunks } from './chunks';
--- content/typescape/basic-types-and-functions/src/utils/strings.ts
+++
@@ -1,6 +0,0 @@
-export function toTitleCase(input: string) {
-  return input
-    .split(/\s+/)
-    .map(p => p.charAt(0).toUpperCase() + p.slice(1))
-    .join(' ');
-}
